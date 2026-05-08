75bc9a10fe58b8d035eb0372c9f07fad7791e8c1
Dac Khai <luongdangdackhai@gmail.com>
2026-05-08 22:07:11 +0700

    create web

---
+++ testappKhai/App test/App test/Load HSK.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+
+
+namespace App_test
+{
+    public class HSK
+    {
+        public string simplified { get; set; }
+        public int frequency { get; set; }
+
+    }
+    
+    internal class Load_hsk
+    {
+        string file = "complete.json";
+        List<HSK> listhsk = new List<HSK>();
+
+        public  List<HSK> GetHSK()
+        {
+           
+            var jsondata = File.ReadAllText(file);
+            Console.Write(GetType(jsondata[0]));
+            var option = new JsonSerializerOptions
+            {
+                AllowTrailingCommas = true,
+            };
+            listhsk = JsonSerializer.Deserialize<List<HSK>>(jsondata, option);
+
+ 
+            
+            return listhsk;
+        }
+
+    }
+} 
---
+++ testappKhai/App test/App test/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace App_test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        Load_hsk loadhsk = new Load_hsk();
+        private void ReloadData() => lvHSK.ItemsSource = loadhsk.GetHSK();
+        private void LoadData(object sender, RoutedEventArgs e) => ReloadData();
+    }
+}
